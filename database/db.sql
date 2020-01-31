4843e6edccd6768eb3f504d664a3919de877ba4b
jamalroger <belharradi.j@gmail.com>
2020-01-31 08:38:55 -0500

    .

---
+++ database/db.sql
@@ -0,0 +1,134 @@
+Ctrl-C -- exit!
+Ctrl-C -- exit!
+-- MySQL dump 10.17  Distrib 10.3.20-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: sms
+-- ------------------------------------------------------
+-- Server version	10.3.20-MariaDB-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `departements`
+--
+
+DROP TABLE IF EXISTS `departements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `departements` (
+  `code` int(11) NOT NULL AUTO_INCREMENT,
+  `lib` text DEFAULT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departements`
+--
+
+LOCK TABLES `departements` WRITE;
+/*!40000 ALTER TABLE `departements` DISABLE KEYS */;
+INSERT INTO `departements` VALUES (1,'informatique'),(2,'gestion');
+/*!40000 ALTER TABLE `departements` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `etudiants`
+--
+
+DROP TABLE IF EXISTS `etudiants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `etudiants` (
+  `cne` char(255) NOT NULL,
+  `nom` text DEFAULT NULL,
+  `prenom` text DEFAULT NULL,
+  `codefil` int(11) DEFAULT NULL,
+  `tele` text DEFAULT NULL,
+  PRIMARY KEY (`cne`),
+  KEY `codefil` (`codefil`),
+  CONSTRAINT `etudiants_ibfk_2` FOREIGN KEY (`codefil`) REFERENCES `filiere` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etudiants`
+--
+
+LOCK TABLES `etudiants` WRITE;
+/*!40000 ALTER TABLE `etudiants` DISABLE KEYS */;
+INSERT INTO `etudiants` VALUES ('1','omar','hasrouf',1,'0613900018'),('2','belharradi','jamal',2,'0613900018'),('k130304056','hamid','jaafer',2,'0660234741');
+/*!40000 ALTER TABLE `etudiants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `filiere`
+--
+
+DROP TABLE IF EXISTS `filiere`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filiere` (
+  `code` int(11) NOT NULL AUTO_INCREMENT,
+  `codeDep` int(11) NOT NULL,
+  `lib` text DEFAULT NULL,
+  PRIMARY KEY (`code`),
+  KEY `codeDep` (`codeDep`),
+  CONSTRAINT `filiere_ibfk_1` FOREIGN KEY (`codeDep`) REFERENCES `departements` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `filiere`
+--
+
+LOCK TABLES `filiere` WRITE;
+/*!40000 ALTER TABLE `filiere` DISABLE KEYS */;
+INSERT INTO `filiere` VALUES (1,1,'developememt web et logiciel'),(2,1,'development mobile');
+/*!40000 ALTER TABLE `filiere` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'jamal','root');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-31  8:36:38
